3d8db29924c41d7dfb07eee87e32f97231e11088
Shikhar Mishra <shikharmishra2001@gmail.com>
2023-07-20 14:10:17 +0530

    Your commit message

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://shikhar302001.github.io/Portfolio/"
+        > Shikhar Mishra</a
+      >, <a href="">Open sourced on Github.</a> Hosted on Github
+    </p>
+  </div>
+  );
+}
